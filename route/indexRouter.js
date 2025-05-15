46bbed54263cdaa3bcc4823375380c8e0af28fa8
ilathem <isaiahlathem@gmail.com>
2025-05-15 10:26:14 -0500

    add sample messages data and display them

--- route/indexRouter.js
+++ route/indexRouter.js
@@ -1,9 +1,30 @@
-const { Router } = require('express');
+// use Router from express
+const { Router } = require("express");
 
+// instantiate the router
 const indexRouter = Router();
 
-indexRouter.get('/', (req, res) => {
-  res.render('index');
+// sample data
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+// tell the router to display the template
+indexRouter.get("/", (req, res) => {
+  res.render("index", {
+    title: "Mini Messenger",
+    messages: messages,
+  });
 });
 
+// export the router
 module.exports = indexRouter;
