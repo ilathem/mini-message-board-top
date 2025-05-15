76c37ee80f406d76c77ca6316ebd53e9851006c0
ilathem <isaiahlathem@gmail.com>
2025-05-15 10:40:32 -0500

    implement redirect after submit

--- route/newMessageRouter.js
+++ route/newMessageRouter.js
@@ -13,6 +13,7 @@ newMessageRouter.post('/', (req, res) => {
   const name = req.body.name;
   const message = req.body.message;
   messages.push({ text: message, user: name, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = newMessageRouter;
